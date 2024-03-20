c3e89b32fd0b4cde754e0152b89eb3398d3d25d0
PaiIan722 <ian2120812@gmail.com>
2024-03-20 12:02:56 +0800

    week05

---
+++ week05/week05-2b.cpp
@@ -0,0 +1,11 @@
+///從 week05-1.cpp 複製過來 week05-2a.cpp
+///Settings-Ediotr (左邊第7個)Sorurce Formatter
+///選 K&R (C的爸爸)
+///CodeBlocks右鍵跳出視窗, Format use AStyle
+#include <stdio.h>
+int main()
+{
+    for(int i=0; i<3; i++) {
+        printf("i是%d", i);
+    }
+}
