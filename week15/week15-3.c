b9af7cd4b0bc5a1a4f4beffad143ec9aa490e8d6
PaiIan722 <ian2120812@gmail.com>
2024-05-29 12:01:34 +0800

    week15

---
+++ week15/week15-3.c
@@ -0,0 +1,13 @@
+int equalSubstring(char* s, char* t, int maxCost) {
+    int N = strlen(s); //先知道字串的長度
+    int ans = 0,j = 0;
+    for(int i=0;i<N;i++){ //字串的迴圈
+        maxCost -= abs( s[i]- t[i] ); //C語言直接減i頭
+        while(maxCost<0){
+            maxCost += abs( s[j] - t[j] ); //小心,是j尾巴
+            j += 1;
+        }
+        if(i-j+1>ans) ans = i-j+1;//更新答案
+    }
+    return ans;//課堂作業1,先隨便給個0當答案(答案當然不對)
+}
